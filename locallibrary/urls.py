2737761dfca073dc2b25478f2c4ebbf9cba44aea
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-01 15:36:13 +0530

    Add support for Heroku deployment
    1. Add STATIC_ROOT in settings.py
    2. Add whitenoise in MIDDLEWARE list
    3. Create 'Procfile'
    4. Create new requirements.txt file using pip3 freeze > requirements.txt
    5. Add path for STATIC files for Heroku Deployment

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,8 +19,14 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
+
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
