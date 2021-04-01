82f2c6f4abea33b763bfc9a0ee5d0a9f7f0ccf8e
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-01 14:35:28 +0530

    Add new record in Catalog app

---
+++ sample_data.py
@@ -0,0 +1,47 @@
+list_of_books = [
+["Ordinary Differential Equations",
+ "Dr. Deepjyoti Kalita",
+ "We live in a day of superhero movies where we see scrawny young men go into a machine and come out super soldiers. It seems like science fiction to think that a similar improvement to the human condition could be possible. But we also are in a time of amazing technological and medical advancements that now make the idea of a real-life Captain America not so far-fetched.",
+ "978-93-89474-49-7",
+ "English",
+ "Motivational"],
+
+["Animal Physiology Controlling and Coordinating Systems",
+ "Dr. Akshay Kumar Haloi",
+ "21 Days To A Big Idea shows you how to combine the creative and rational sides of your brain to come up with cool, new ideas and fun ways to implement them, which might even help you create a sustainable business in the long run, in as little as 21 days.",
+ "978-93-89474-48-0",
+ "English",
+ "Educational"],
+
+["Narangian	Assamese	1	First",
+ "Reeta Dutta Hazorika",
+ "A Brief History Of Everyone Who Ever Lived gives you another important perspective on mankind’s past and present through the lens of our genes.",
+ "978-93-90207-00-8",
+ "English",
+ "Motivational"],
+
+["Trends in English Literature",
+"Salinder Ranga",
+ "30 Lessons For Loving gives the relationship advice of hundreds of couples who have stayed together into old age and will teach you how to have happiness and longevity in your love life.",
+ "978-93-86695-33-8",
+ "English",
+ "Fantasy"],
+
+["Bhartiya Sangeet Avem Sanskriti (Hindi)",
+ "Geeta Sharma Pathak",
+ "60 Seconds & You’re Hired! is a guide to getting your dream job that will help you feel confident in your next interview by teaching you how to impress your interviewer with being concise, focusing on your strengths, and knowing what to do at every step of the process.",
+ "978-93-86695-32-1",
+ "English",
+ "Fiction"],
+]
+
+
+# CODE TO SAVE DATA
+# for x in list_of_books:
+# ...     title = x[0]
+# ...     author = x[1]
+# ...     summary = x[2]
+# ...     isbn = x[3]
+# ...     genre = x[4]
+# ...     book_obj = Book(title=title, author=author, summary=summary, isbn=isbn, genre=genre)
+# ...     book_obj.save()
