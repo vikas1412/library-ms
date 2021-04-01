43d5401b6789e8b5253969b3cacdfabdf4d6d9d0
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-01 13:22:23 +0530

    Organize detail view layout
    Use 'fields' to organise 'date_of_birth' & 'date_of_death' vertically, control which fields to show in Admin interface

--- catalog/admin.py
+++ catalog/admin.py
@@ -12,7 +12,7 @@ admin.site.register(Language)
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-# admin.site.register(Author, AuthorAdmin)
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 
 @admin.register(Book)
