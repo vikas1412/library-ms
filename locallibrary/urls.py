d8f85c30a1eea1f5f386e53760db1c160774a0b0
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-01 15:42:13 +0530

    Import serve in project urls.py & remove duplicate imports

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
