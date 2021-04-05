5a79a7b861a8858dfc12a4419b31963116763e80
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-05 11:28:23 +0530

    Add path to BookListView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from catalog import views
+from catalog.views import BookListView
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('all-books/', views.all_books, name="all_books"),
-    path('all-authors/', views.all_authors, name="all-authors"),
+    path('books/', BookListView.as_view(), name="books"),
+
+    path('authors/', views.all_authors, name="authors"),
 ]
